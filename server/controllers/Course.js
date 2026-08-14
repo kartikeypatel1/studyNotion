56b204c9f4f7a2b431c2dcf057613466f2fdcc6b
kartikey <kartikey40patel@gmail.com>
2026-08-15 02:10:28 +0530

    create the createCourse controller function

--- server/controllers/Course.js
+++ server/controllers/Course.js
@@ -7,7 +7,19 @@ const {uploadImageCloudinary}=require("../utils/imageUploader");
 
 exports.createCourse=async(req,res)=>{
     try{
-        
+        //fetch the data 
+        const{courseName,courseDescription,price,tag}=req.body;
+
+        //get the thumbnail
+        const thumbnail=req.files.thumbnailImage;
+
+        //validation
+        if(!courseName||!courseDescription||!whatYouWillLearn||!price||!tag){
+            return res.status(500).json({
+                success:false,
+                message:error.message,
+            })
+        }
     }catch(err){
         return res.status(500).json({
             success:false,
