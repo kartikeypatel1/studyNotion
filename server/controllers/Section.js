39fc2af0c4fe0e466c69609c30bad53baaa1b299
kartikey <kartikey40patel@gmail.com>
2026-08-16 04:21:39 +0530

    create section, deleteSection ,UpdateSection controller function write

--- server/controllers/Section.js
+++ server/controllers/Section.js
@@ -1,6 +1,7 @@
 const Section=require('../models/Section');
 const Course=require('../models/Course');
-const SubSection=require('../models/SubSection')
+const SubSection=require('../models/SubSection');
+const { section } = require('framer-motion/client');
 exports.createSection=async(req,res)=>{
     try{
         //fetch the data
@@ -48,4 +49,66 @@ exports.createSection=async(req,res)=>{
         });
     }
     
+}
+
+
+//update section controller handler fucntion
+
+exports.updateSection=async(req,res)=>{
+    try{
+        //data fetch
+        const{sectionName,sectionId}=req.body;
+        //data validation
+        if(!sectionName){
+            return res.status(400).json({
+                success:false,
+                message:'Properties are missing',
+            });
+        }
+        //update data
+
+        const updateSection=await Section.findByIdAndUpdate(sectionId,{sectionName},{new:true});
+        //return res.
+        return res.status(200).json({
+            success:true,
+            message:'Section updated Successfully',
+        });
+    }catch(err){
+        return res.status(500).json({
+            success:false,
+            message:"Unable update the section, please try again",
+            error:err.message,
+        })
+    }
+};
+
+//delete the section
+
+exports.deleteSection=async(req,res)=>{
+    try{
+        //get id -assuming that we are send ing ID in params
+        const{sectionId}=req.params;
+
+        //data validate
+
+        if(!sectionId){
+            return res.status(400).json({
+                success:false,
+                message:'Properties are missing',
+            });
+        }
+        //data delete
+        await Section.findByIdAndDelete(sectionId);
+        //return response
+        return res.status(200).json({
+            success:true,
+            message:'Section deleted Successfully',
+        });
+    }catch(err){
+        return res.status(500).json({
+            success:false,
+            message:'Try again later',
+            error:err.message,
+        })
+    }
 }
