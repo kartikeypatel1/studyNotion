3fe5cc370d32b5f68512a5f6ee14726499107702
kartikey <kartikey40patel@gmail.com>
2026-08-16 18:15:12 +0530

    update profile handler function write

--- server/controllers/Profile.js
+++ server/controllers/Profile.js
@@ -5,16 +5,44 @@ const User=require('../models/User');
 exports.updateProfile=async(req,res)=>{
     try{
         //get datae
-        
+        const{dateOfBirth="",about="",contactNumber,gender}=req.body;
+
         //get userid
+        const id=req.User.id;
+
         //validation
+        if(!contactNumber||!gender){
+            return res.status(400).json({
+                success:false,
+                message:"all fields are required",
+            })
+        }
+
         //find profile
+
+        const userDetails=await User.findById(id);
+        const profileId=userDetails.additionalDetails;
+        const profileDetails=await Profile.findById(profileId);
+
+
         //update profile
+        profileDetails.dateOfBirth=dateOfBirth;
+        profileDetails.about=about;
+        profileDetails.contactNumber=contactNumber;
+        profileDetails.gender=gender;
+        await profileDetails.save();
         //return response
+        return res.status(200).json({
+            success:true,
+            message:"Profile details are successfully updated",
+            profileDetails,
+        })
 
     }catch(err){
         return res.status(500).json({
-
-        })
+            success:false,
+            message:'server error',
+            error:err.message,
+        });
     }
 }
