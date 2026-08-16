c3462bb1d10753e2e904bf2fb7ff43ef3bc16f74
kartikey <kartikey40patel@gmail.com>
2026-08-16 18:04:39 +0530

    create the profile controller

---
+++ server/controllers/Profile.js
@@ -0,0 +1,20 @@
+const Profile=require('../models/Profile');
+const User=require('../models/User');
+
+
+exports.updateProfile=async(req,res)=>{
+    try{
+        //get datae
+        
+        //get userid
+        //validation
+        //find profile
+        //update profile
+        //return response
+
+    }catch(err){
+        return res.status(500).json({
+
+        })
+    }
+}
