5f6c1dd011dc2b95cfa3184a92afa7d71c14d05a
kartikey <kartikey40patel@gmail.com>
2026-08-16 18:38:27 +0530

    update profile handler function write

--- server/controllers/Profile.js
+++ server/controllers/Profile.js
@@ -46,3 +46,61 @@ exports.updateProfile=async(req,res)=>{
         });
     }
 }
+
+
+exports.deleteProfile = async (req, res) => {
+    try {
+        const id = req.User.id;
+
+        // Find user
+        const userDetails = await User.findById(id);
+
+        if (!userDetails) {
+            return res.status(400).json({
+                success: false,
+                message: "User not found",
+            });
+        }
+
+        // Remove user from all enrolled courses
+        await Course.updateMany(
+            {
+                studentsEnrolled: id
+            },
+            {
+                $pull: {
+                    studentsEnrolled: id
+                }
+            }
+        );
+
+        // Delete profile
+        await Profile.findByIdAndDelete(
+            userDetails.additionalDetails
+        );
+
+        // Delete user
+        await User.findByIdAndDelete(id);
+
+        return res.status(200).json({
+            success: true,
+            message: "Account deleted successfully",
+        });
+
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "User cannot deleted",
+            error: err.message,
+        });
+    }
+};
+
+
+exports.getAllUserDetails=async(req,res)=>{
+    try{
+        
+    }catch(err){
+
+    }
+}
