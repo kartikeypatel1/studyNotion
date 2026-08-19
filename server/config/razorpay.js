05edc807283481c16214bb25e7f0aa629be5e28d
kartikey <kartikey40patel@gmail.com>
2026-08-20 03:45:25 +0530

    create instance

---
+++ server/config/razorpay.js
@@ -0,0 +1,7 @@
+const { progressPercentage } = require('framer-motion');
+require('dotenv').config();
+const Razorpay=require('razorpay');
+exports.instance=new Razorpay({
+    key_id:process.env.RAZORPAY_KEY,
+    key_secret:process.env.RAZORPAY_SECRET,
+})
