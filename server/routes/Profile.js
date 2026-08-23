d77c14ad1ef1738e78031e1bbd9de6869483e839
kartikey <kartikey40patel@gmail.com>
2026-08-23 16:44:25 +0530

    create profile route

--- server/routes/Profile.js
+++ server/routes/Profile.js
@@ -1,25 +1,25 @@
-// const express = require("express")
-// const router = express.Router()
-// const { auth, isInstructor, isStudent } = require("../middlewares/auth")
-// const {
-//   deleteAccount,
-//   updateProfile,
-//   getUserAllDetails,
-//   updateDisplayPicture,
-//   getEnrolledCourses,
-//   instructorDashboard,
-// } = require("../controllers/profile")
+const express = require("express")
+const router = express.Router()
+const { auth, isInstructor, isStudent } = require("../middlewares/auth")
+const {
+  deleteAccount,
+  updateProfile,
+  getUserAllDetails,
+  updateDisplayPicture,
+  getEnrolledCourses,
+  instructorDashboard,
+} = require("../controllers/profile")
 
-// // ********************************************************************************************************
-// //                                      Profile routes
-// // ********************************************************************************************************
-// // Delet User Account
-// router.delete("/deleteProfile", auth, deleteAccount)
-// router.put("/updateProfile", auth, updateProfile)
-// router.get("/getUserDetails", auth, getUserAllDetails)
-// // Get Enrolled Courses
-// router.get("/getEnrolledCourses", auth,isStudent, getEnrolledCourses)
-// router.put("/updateDisplayPicture", auth, updateDisplayPicture)
-// router.get("/instructorDashboard", auth, isInstructor, instructorDashboard)
+// ********************************************************************************************************
+//                                      Profile routes
+// ********************************************************************************************************
+// Delet User Account
+router.delete("/deleteProfile", auth, deleteAccount)
+router.put("/updateProfile", auth, updateProfile)
+router.get("/getUserDetails", auth, getUserAllDetails)
+// Get Enrolled Courses
+router.get("/getEnrolledCourses", auth,isStudent, getEnrolledCourses)
+router.put("/updateDisplayPicture", auth, updateDisplayPicture)
+router.get("/instructorDashboard", auth, isInstructor, instructorDashboard)
 
-// module.exports = router
+module.exports = router
